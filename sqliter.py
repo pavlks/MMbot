55ac3738d4998fca8ca75dd0975715edd9499636
Pavel Sazonov <sazonov.ps@pm.me>
2020-11-20 15:58:25 -0500

    messages style adjunstments

--- sqliter.py
+++ sqliter.py
@@ -42,8 +42,7 @@ class Client(Base):
         for row in query_result:
             b_date = date.fromisoformat(row.birth_date).strftime('%d-%b-%Y')
             s_date = date.fromisoformat(row.start_date).strftime('%d/%m')
-            processed_list.append(f'/id{row.id} <code>{row.fullname} {b_date}</code>     с {s_date}')
-            # processed_list.append(f'<b>{row.fullname}</b>  {b_date}  <code>с {s_date}</code>  /id{row.id}')
+            processed_list.append(f'/id{row.id} <b>{row.fullname}</b>  {b_date}    <i>с {s_date}</i>')
         result = '\n\n'.join(processed_list) + '\n\n' + F'Всего: {count_results} чел.'
         session.close()
         return result
@@ -78,7 +77,7 @@ class Client(Base):
         for row in query_result:
             b_date = date.fromisoformat(row.birth_date).strftime('%d-%b-%Y')
             s_date = date.fromisoformat(row.start_date).strftime('%d/%m')
-            processed_list.append(f'/id{row.id} <code>{row.fullname} {b_date}</code>     с {s_date}')
+            processed_list.append(f'/id{row.id} <b>{row.fullname}</b>  {b_date}    <i>с {s_date}</i>')
         result = '\n\n'.join(processed_list) + '\n\n' + F'Всего: {count_results} чел.'
         session.close()
         return result
@@ -171,7 +170,7 @@ class Client(Base):
             for row in query_result:
                 b_date = date.fromisoformat(row.birth_date).strftime('%d-%b-%Y')
                 s_date = date.fromisoformat(row.start_date).strftime('%d/%m')
-                processed_list.append(f'/id{row.id} <code>{row.fullname} {b_date}</code>     с {s_date}')
+                processed_list.append(f'/id{row.id} <b>{row.fullname}</b>  {b_date}    <i>с {s_date}</i>')
             result = '\n\n'.join(processed_list)
         session.close()
         return result, total_results, client_id, client_status, photo  # will send tuple (result, total_results, row.id). When found 1 record row.id will serve us for buttons,
